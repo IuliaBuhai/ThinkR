aa0f12217fd2d3afe95a4747532dea0d99a75fc4
IuliaBuhai <137499884+IuliaBuhai@users.noreply.github.com>
2025-05-23 18:18:25 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,38 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDXwRE-7Cxr6YKE6MPKB7m9aC4xbirkRVY",
+  authDomain: "thinkr-298dd.firebaseapp.com",
+  projectId: "thinkr-298dd",
+  storageBucket: "thinkr-298dd.firebasestorage.app",
+  messagingSenderId: "1087091426997",
+  appId: "1:1087091426997:web:4e080caec0181377aa60fc",
+  measurementId: "G-N20TE0F9VT"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+document.getElementById('signup').onclick = () => {
+  const email = emailInput.value;
+  const password = passwordInput.value;
+  createUserWithEmailAndPassword(auth, email, password)
+    .then(() => alert("Signed up!"))
+    .catch(err => alert(err.message));
+};
+
+document.getElementById('login').onclick = () => {
+  const email = emailInput.value;
+  const password = passwordInput.value;
+  signInWithEmailAndPassword(auth, email, password)
+    .then(() => alert("Logged in!"))
+    .catch(err => alert(err.message));
+};
+
+document.getElementById('logout').onclick = () => {
+  signOut(auth).then(() => alert("Logged out"));
+};
+
+const emailInput = document.getElementById('email');
+const passwordInput = document.getElementById('password');
---
+++ dashboard.js
@@ -0,0 +1,40 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAuth, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { getFirestore, doc, setDoc } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDXwRE-7Cxr6YKE6MPKB7m9aC4xbirkRVY",
+  authDomain: "thinkr-298dd.firebaseapp.com",
+  projectId: "thinkr-298dd",
+  storageBucket: "thinkr-298dd.firebasestorage.app",
+  messagingSenderId: "1087091426997",
+  appId: "1:1087091426997:web:4e080caec0181377aa60fc",
+  measurementId: "G-N20TE0F9VT"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+onAuthStateChanged(auth, user => {
+  if (!user) {
+    alert("You must be logged in to view this page.");
+    window.location.href = "auth.html";
+  } else {
+    document.getElementById('output').innerText = "Welcome, " + user.email;
+  }
+});
+
+document.getElementById('saveData').onclick = async () => {
+  const user = auth.currentUser;
+  const data = document.getElementById('userData').value;
+  await setDoc(doc(db, "users", user.uid), { data });
+  alert("Data saved to Firestore!");
+};
+
+document.getElementById('logout').onclick = () => {
+  signOut(auth).then(() => {
+    alert("Logged out");
+    window.location.href = "auth.html";
+  });
+};
